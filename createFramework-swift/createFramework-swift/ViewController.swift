a08eca94e34de8795b31e9fa9c8e9f0a4d67e850
viktor hugo <victorHbenitez85@hotmail.com>
2018-08-28 16:32:06 -0500

    URW Challenge import framework and use it

---
+++ createFramework-swift/createFramework-swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  createFramework-swift
+//
+//  Created by Victor Hugo Benitez Bosques on 28/08/18.
+//  Copyright © 2018 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+import Checkbox
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var checkbox: Checkbox!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+}
+
